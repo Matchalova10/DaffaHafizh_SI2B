02dd1331c8c92eefa65606df55a3082575f5ffd6
DaffaHafizhRamdhan19 <133848687+Matchalova10@users.noreply.github.com>
2023-09-02 13:11:05 +0700

    Add files via upload

---
+++ latihan3.3.php
@@ -0,0 +1,25 @@
+<?php
+class TOKOPEGADAIAN {
+    var $Nama_Toko = "TOKO PEGADAIAN";
+    var $Alamat_Toko = "jl Keadilan No 16";
+    var $No_tlpn =  72353459;
+
+    var $Besar_Pinjaman = 1000000;
+    var $Besaran_Bunga = 10;
+    var $Lama_Angsuran = 5;
+
+public function Total_Seluruh () {
+    $totalPinjaman = $this -> Besar 
+    $besarAngsuran = $totalPinjaman
+
+    echo "Besar Pinjaman : Rp. " . n
+    echo "Besar Bunga : ". $this ->
+    echo "Lama Angsuran : ". $this 
+    echo "Besar Angsuran per Bulan : "
+    echo "Total Peminjaman : Rp. " . n   
+    }
+}
+
+$objekToko = new TOKOPEGADAIAN ();
+$objekToko = TotalSeluruh ();
+    
